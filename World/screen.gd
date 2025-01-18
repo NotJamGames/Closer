4450ddc39516c7e70167f386c97ac9f0a0536ab1
= <harrisonkaye93@gmail.com>
2025-01-18 22:03:05 +0000

    aesthetic changes, intro animation, credits

---
+++ World/screen.gd
@@ -0,0 +1,20 @@
+extends MeshInstance3D
+
+
+@export var crt_startup : AudioStreamPlayer
+
+
+func power_on() -> void:
+	crt_startup.play()
+
+	mesh.material.set("shader_parameter/brightness", 8.0)
+	set_surface_override_material(0, null)
+
+	var tween : Tween = get_tree().create_tween()
+	tween.tween_method\
+			(set_brightness, mesh.material.get("shader_parameter/brightness"),
+			1.0, 1.8).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+
+
+func set_brightness(new_value : float) -> void:
+	mesh.material.set("shader_parameter/brightness", new_value)
--- World/world.gd
+++ World/world.gd
@@ -1,12 +1,27 @@
 extends Node3D
 
 
+@export var screen : MeshInstance3D
 @export var subviewport : SubViewport
+@export var blackout : ColorRect
+
+@export var black_screen : Resource
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
+	screen.set_surface_override_material(0, black_screen)
+	var timer : SceneTreeTimer = get_tree().create_timer(4.0)
+	timer.timeout.connect\
+			(screen.power_on)
+
 
 func _input(event: InputEvent) -> void:
 	subviewport.push_input(event)
+
+
+func fade_in(duration : float) -> void:
+	var tween : Tween = get_tree().create_tween()
+	tween.tween_property(blackout, "color:a", .0, duration)\
+			.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
